7b07b2e1e01c850c389b8ee8f5a322d9bfba096f
kimmarzo <kjrmarzo@gmail.com>
2019-04-21 12:18:25 -0800

    no message

---
+++ BEACHHACKS'19/Assets/noCube.cs
@@ -0,0 +1,85 @@
+﻿//======= Copyright (c) Valve Corporation, All rights reserved. ===============
+//
+// Purpose: Target that sends events when hit by an arrow
+//
+//=============================================================================
+
+using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+namespace Valve.VR.InteractionSystem
+{
+	//-------------------------------------------------------------------------
+	public class noCube : MonoBehaviour
+	{
+        public ParticleSystem explosion;
+		public bool onceOnly = false;
+		public Transform targetCenter;
+
+		public Transform baseTransform;
+		public Transform fallenDownTransform;
+		public float fallTime = 0.5f;
+        public GameObject parentWhole;
+		const float targetRadius = 0.25f;
+
+		private bool targetEnabled = true;
+
+
+		//-------------------------------------------------
+		private void ApplyDamage()
+		{
+			OnDamageTaken();
+		}
+
+
+		//-------------------------------------------------
+		private void FireExposure()
+		{
+			OnDamageTaken();
+           
+		}
+
+
+		//-------------------------------------------------
+		private void OnDamageTaken()
+		{
+            explosion.Emit(100);
+            Application.Quit();
+            //if ( targetEnabled )
+            //{
+            //	onTakeDamage.Invoke();
+            //	StartCoroutine( this.FallDown() );
+
+            //	if ( onceOnly )
+            //	{
+            //		targetEnabled = false;
+            //	}
+            //}
+        }
+
+
+		////-------------------------------------------------
+		//private IEnumerator FallDown()
+		//{
+		//	if ( baseTransform )
+		//	{
+		//		Quaternion startingRot = baseTransform.rotation;
+
+		//		float startTime = Time.time;
+		//		float rotLerp = 0f;
+
+		//		while ( rotLerp < 1 )
+		//		{
+		//			rotLerp = Util.RemapNumberClamped( Time.time, startTime, startTime + fallTime, 0f, 1f );
+		//			baseTransform.rotation = Quaternion.Lerp( startingRot, fallenDownTransform.rotation, rotLerp );
+		//			yield return null;
+		//		}
+		//	}
+
+		//	yield return null;
+		//}
+	}
+}
