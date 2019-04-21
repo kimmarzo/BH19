3bd2a21451429fe0ccdd5f8663d10907edc141ee
kimmarzo <kjrmarzo@gmail.com>
2019-04-21 07:23:36 -0800

    idk anymore, just a lot

---
+++ BEACHHACKS'19/Assets/TutToLevel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutToLevel : MonoBehaviour
+{
+    public GameObject tutCube1;
+    public GameObject tutCube2;
+    public GameObject tutCube3;
+    public GameObject leftGate;
+    public GameObject rightGate;
+    bool openGates = false;
+    public float gateSpeed = .5f;
+    public float waitTimeForGate = 3f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tutCube1.activeInHierarchy == false && tutCube2.activeInHierarchy == false && tutCube3.activeInHierarchy == false)
+        {
+            openGates = true;
+            StartCoroutine(initializingGate());
+        }
+    }
+
+    IEnumerator initializingGate()
+    {
+        yield return new WaitForSeconds(waitTimeForGate);
+        leftGate.transform.position += Vector3.left * Time.deltaTime * gateSpeed;
+        rightGate.transform.position += Vector3.right * Time.deltaTime * gateSpeed;
+
+        Debug.Log("OPEN GATE PLTHHHHHHHHHHH");
+    }
+}
--- BEACHHACKS'19/Assets/longBowDesk.cs
+++ BEACHHACKS'19/Assets/longBowDesk.cs
@@ -5,15 +5,35 @@ using UnityEngine;
 public class longBowDesk : MonoBehaviour
 {
     public GameObject Weapon;
-    public float Speed = 0.2f;
+    public float SpeedTable = 0.2f;
+    public float SpeedTutCubes = 0.7f;
     public GameObject BowTableSystem;
+    public GameObject tutorialCubes;
+    public float tutCubeHeight = 15f;
+    public bool isHolding = false;
+    public GameObject LongBowSystem;
+
     private void Update()
     {
        if (Weapon.GetComponent<Valve.VR.InteractionSystem.ItemPackageSpawner>().justPickedUpItem == true)
         {
-       
-            transform.position += Vector3.down * Time.deltaTime * Speed;
-        
+
+            isHolding = true;
         }
+
+        if (isHolding == true)
+        {
+  
+            transform.position += Vector3.down* Time.deltaTime* SpeedTable;
+            if (tutorialCubes.transform.position.y < tutCubeHeight)
+            {
+                tutorialCubes.transform.position += Vector3.up * Time.deltaTime * SpeedTutCubes;
+                BowTableSystem.transform.position += Vector3.down * Time.deltaTime * SpeedTable;
+            }
+
+        }
+    
+
+
     }
 }
