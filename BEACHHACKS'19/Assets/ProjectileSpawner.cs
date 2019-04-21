856c761c7e8c3247328c3b1acbc2b7e20a3bb683
kimmarzo <kjrmarzo@gmail.com>
2019-04-21 08:57:01 -0800

    - Added prefabs

---
+++ BEACHHACKS'19/Assets/ProjectileSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileSpawner : MonoBehaviour
+{
+    public GameObject[] targetCubes;
+    public Vector3 spawnValues;
+    public float spawnWait;
+    public int startWait;
+    public float spawnMostWait;
+    public float spawnLeastWait;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator waitSpawner ()
+    {
+        yield return new WaitForSeconds(startWait);
+
+       
+    }
+}
