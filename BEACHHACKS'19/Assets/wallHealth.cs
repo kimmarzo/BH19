a10c63e0d377a8e5920c9b2f6bb2709b6fe91129
kimmarzo <kjrmarzo@gmail.com>
2019-04-20 20:54:44 -0800

    - Fixed cubes

---
+++ BEACHHACKS'19/Assets/wallHealth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wallHealth : MonoBehaviour
+{
+    public float health = 100;
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "projectile")
+        {
+            health -= 10;
+            Destroy(other.gameObject);
+        }
+    }
+}
